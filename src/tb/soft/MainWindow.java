d37c40aba51578efa6bbe5886bc42d3643e57b86
Marcin Gnap <258953@student.pwr.edu.pl>
2021-12-13 16:41:33 +0100

    Commit v4 got sth pointer

--- src/tb/soft/MainWindow.java
+++ src/tb/soft/MainWindow.java
@@ -79,22 +79,6 @@ public class MainWindow extends JFrame {
     }
 
     public void runCanva(){
-
+        MyCanva.drawOval();
     }
-}
-
-class MyCanva extends Canvas{
-
-    Point p = MouseInfo.getPointerInfo().getLocation();
-    int MouseX = p.x;
-    int MouseY = p.y;
-
-    public void drawOval(Graphics g, int MouseX, int MouseY){
-        g.drawOval(MouseX, MouseY, 25, 25);
-    }
-
-    public void drawRect(Graphics g, int MouseX, int MouseY){
-        g.drawRect(MouseX, MouseY, 25, 25);
-    }
-
 }
